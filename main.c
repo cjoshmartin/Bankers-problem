897e2b6a7788760b2b8804cadf93991b82b8b8bb
Josh <joshmartin5001@gmail.com>
2019-03-18 22:30:52 -0400

    Created an Init function for Main

--- main.c
+++ main.c
@@ -9,29 +9,47 @@
 #include "resource/resource.h"
 #include "update/update.h"
 #include "safe/safe.h"
+#include "printer.h"
 
+void init(banker _them){
+    int size_i = NUMBER_OF_CUSTOMERS,
+     size_j = NUMBER_OF_RESOURCES;
+
+    srand(time(NULL));
+
+    for(int i = 0; i < size_i; i++){
+        for (int j = 0; j < size_j; j++) {
+            if (_them.available[i] > 0)
+                _them.maximum[i][j] = rand() % _them.available[i]; // allcat. random values to max based on values passed to available[]
+            _them.allocation[i][j] = 0;
+            _them.need[i][j] = 0;
+        }
+    }
+    printer_init(_them);
+    print_all();
+
+    updateAvailable(size_i, _them);
+}
 
 // (4 pts) overall working program
 int main(int argc, char** argv){
+
 // the available amount of each resource
-    srand(time(NULL));
     banker _this;
-    for (int i = 1; i < argc; ++i)
+
+    for ( int i = 1; i < argc; ++i)
         _this.available[i-1] = atoi(argv[i]); // copy resources to av.
 
-    int size = argc - 1;
+    init(_this);
 
-    for(int i = 0; i < size; i++){
-        for (int j = 0; j < size; j++) {
-            _this.maximum[i][j] = rand() % _this.available[i]; // allcat. random values to max based on values passed to available[]
-            _this.allocation[i][j] = rand() % _this.available[i];
-        }
-    }
-    updateAvailable(size, _this);
-    
-    // (4 pts) implementation of customer threads
+
+    return 0;
+}
+
+
+// (4 pts) implementation of customer threads
 //    while(TRUE){
-        isSafe(_this);
+//isSafe(_this);
 //        for (int n = 0; n < NUMBER_OF_CUSTOMERS; n++){
 //            threads[n].customer_num = n;
 //            threads[n].process = available;// TODO: This is probably wrong
@@ -48,8 +66,4 @@ int main(int argc, char** argv){
 //                    );
 //        }
 //    }
-
-    return 0;
-}
-
 
